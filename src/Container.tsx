bf0ab4874cbee376e789fd633281165bf99afb99
Nathan Searles <nsearles@gmail.com>
2021-06-08 17:13:53 -0700

    Added ability to pass class prop into the container

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,18 +1,19 @@
-// TODO: forward ref
-// TODO: pass class name - scoped or non-scoped
 import React, { forwardRef, ReactNode } from 'react';
 import styles from './grid.module.scss';
 
 interface Props {
   children: ReactNode;
+  className?: string;
 }
 
 export type Ref = HTMLDivElement;
 
-const Container = forwardRef<Ref, Props>(({ children }: Props, ref) => (
-  <div ref={ref} className={styles.container}>
-    {children}
-  </div>
-));
+const Container = forwardRef<Ref, Props>(
+  ({ children, className }: Props, ref) => (
+    <div ref={ref} className={[styles.container, className].join(' ')}>
+      {children}
+    </div>
+  ),
+);
 
 export default Container;
